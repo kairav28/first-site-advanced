aa77310c1acfac3f0ce84fdaa83c93e8285e37ad
kairav28 <63807874+kairav28@users.noreply.github.com>
2024-03-07 17:19:36 +0000

    added flask project

---
+++ app.py
@@ -0,0 +1,17 @@
+# import flask
+from flask import Flask
+
+# create flask app
+app = Flask (__name__)
+
+# register a route to the application, here "/" means home page, after that the fuction runs the scrpit to print the text
+@app.route("/")
+
+def hello_world():
+  return "Hello Worl!"
+
+# this is to run the app
+if __name__ == "__main__":
+  # 0.0.0.0 to make it run locally
+  # debug = true so that screen always changes
+  app.run(host = '0.0.0.0', debug=True)
