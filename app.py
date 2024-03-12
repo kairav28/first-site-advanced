6a73edbe0a37f8a9119d8e21cddd03da12346be3
kairav28 <63807874+kairav28@users.noreply.github.com>
2024-03-12 03:40:28 +0000

    added api route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import flask
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 # create flask app
 app = Flask (__name__)
@@ -26,15 +26,19 @@ ANIMALS =[
   }
 ]
 
-
-
-
 # register a route to the application, here "/" means home page, after that the fuction runs the scrpit to print the text
 @app.route("/")
 
 def hello_world():
   # sending database / dictionary to html, by passing argument below
   return render_template ('home-bs.html', animals=ANIMALS)
+
+# now  to get list of animals using a json - jasonify returns the list as a json file - it can be found at the route /api/animals
+@app.route("/api/animals")
+def list_animals():
+  return jsonify(ANIMALS)
+
+
 
 
 # this is to run the app
